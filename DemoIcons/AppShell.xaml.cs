5cf2e965d2ba353339cbafacb324ef64f2c947fe
Netcode-Hub <Rastinga1995@hotmail.com>
2023-08-17 02:57:14 +0000

    Add project files.

---
+++ DemoIcons/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoIcons
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DemoIcons/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DemoIcons
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
